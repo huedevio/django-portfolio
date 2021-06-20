101c267dc387d9177c4497255330b5d53f478b80
Bernd Hülsebusch <contact@huedev.io>
2021-06-20 18:29:33 +0200

    Add apps

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,4 +3,3 @@ from django.contrib import admin
 from .models import Job
 
 admin.site.register(Job)
-
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #   'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'rocha001',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
